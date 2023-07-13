69b70116533075fed55bc5fb10a44acdd3e40dd4
TEjosee <tonyetim2.5@gmail.com>
2023-07-14 00:57:12 +0100

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *Return: always return zero for success
  */
 
-int main(void);
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return(0);	     
 }
