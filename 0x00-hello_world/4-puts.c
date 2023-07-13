02447cafc363e462cfe2691382a4e65385efe9da
TEjosee <tonyetim2.5@gmail.com>
2023-07-14 00:50:07 +0100

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - using puts to print to the standard o
+ *utput
+ *Return: always return zero for success
+ */
+
+int main(void);
+{
+	puts("Programming is like building a multilingual puzzle");
+	
+	return(0);	     
+}
