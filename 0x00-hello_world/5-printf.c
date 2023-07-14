c0fd7b382223101c4d97ac76bc841797b278430e
TEjosee <tonyetim2.5@gmail.com>
2023-07-14 01:27:33 +0100

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf to print to the standard o
+ * Return: always return zero for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
