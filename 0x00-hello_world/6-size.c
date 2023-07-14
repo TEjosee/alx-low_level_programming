3e06b0c127121d00c6dbb547890002178c6d1a8d
TEjosee <tonyetim2.5@gmail.com>
2023-07-14 02:24:50 +0100

    assembler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - using the printf function to print to 
+ * Return: always return zero for success
+ */
+int main(void)
+{
+	char = 'a';
+	int = 'b';
+	long int = 'c'
+	long long int = 'd'
+	float = 'e'
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(f));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+    
+    return 0;
+}
+
