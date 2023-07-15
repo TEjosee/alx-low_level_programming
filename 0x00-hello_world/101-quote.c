41b1985e3a409673688ab8787b77ccc9fe2d7f0f
TEjosee <tonyetim2.5@gmail.com>
2023-07-15 22:37:41 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h> #include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  *main - using putchar to print to std error
  *
- *Return: Always return one for success
+ *Return: Always return zero for success
  *
  */
-int putchar(void)
+int main(void)
 {
-	char ch[80] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(ch);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
