2fc5f88eee4cf69436dfd99bdf7f54e7e4225ef6
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 10:27:54 +0100

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - using putchar to print to the standard o *t
+ * Return: Always return zero for success
+ */
+int main(void)
+{
+        int num;
+
+        for (num = 48; num < 58; num++)
+        {
+                putchar(num);
+        }
+        putchar('\n');
+        return (0);
+}
