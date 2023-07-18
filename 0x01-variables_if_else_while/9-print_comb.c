3dc3cc4497305631d3deda59f47e8631057a6a21
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 11:51:15 +0100

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - using putchar to print to the standard o *t
+ * Return: Always return zero for success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
