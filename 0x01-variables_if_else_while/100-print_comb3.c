7a20e1afdfe8e641904ee5d5d8222073f609b973
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 22:42:55 +0100

    putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - using putchar to print to the standard o *t
+ * Return: Always return zero for success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 01; num < 90; num++)
+	{
+		putchar(num);
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - using printf to print to the stdout
+ *main - using printf to print to the std output
  *Return: Always zero for success
  *
  */
