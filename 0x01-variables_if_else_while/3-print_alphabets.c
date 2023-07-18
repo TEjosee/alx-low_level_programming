8c8a0d2f3b914dbde21cdb68bf66a9a5fcf5edaf
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 03:28:43 +0100

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - using putchar function to print to 
+ * Return: Always zero for success
+ */
+int main (void)
+{
+	char upper, lower;
+
+	for (upper = 'a'; upper <= 'z'; upper++)
+	{
+			putchar(upper);
+	}
+	for (lower = 'A'; lower <= 'Z'; lower++)
+	{
+			putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
