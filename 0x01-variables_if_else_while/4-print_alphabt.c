371822edabf6f437bfa5a49ab94ef243f85f086c
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 04:25:15 +0100

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - using putchar function to print to
+ * Return: Always zero for success
+ */
+int main(void)
+{
+        char ch = 'a';
+
+	while (ch <= 'z')
+        {
+		if (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+	}
+	ch++;
+	}
+
+        putchar('\n');
+        return (0);
+}
