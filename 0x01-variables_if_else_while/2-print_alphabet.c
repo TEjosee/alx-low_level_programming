4547de0c2ccb45e1272c2cbfb3ce9913d099eb6f
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 02:52:25 +0100

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 /**
- * main - using putchar to print to the standard out
+ * main - using putchar to print to the standard o *t
+ * Return: Always return zero for success
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - using putchar to print to the standard outpu
+ *t
+ * Return: Always return zero for success
+ */
+int main(void);
+        char alphabeth;
+        alphabeth = 'a';
+{
+        if (alphabeth = a, alphabeth <= z, a++:);
+        putchar(alphabeth\n);
+        return (0);
+}
