22cd17a18e58132cacc6c47fd311845d93ff6947
TEjosee <tonyetim2.5@gmail.com>
2023-07-18 09:33:38 +0100

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - calling printf from the stdio
+ *
+ *Return: Always return zero for success
+ */
+int main(void)
+{
+	int a;
+	
+	for(a = 0; a <= 10; a = a + 1)
+	printf("a is %d\n", a);
+	return (0);
+}
