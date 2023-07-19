7afd9b0454aa5029d738d06d3821b9d97292a7e3
TEjosee <tonyetim2.5@gmail.com>
2023-07-19 17:09:27 +0100

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
  *_islower - checking Ascii value for c if lower or up
  *
  *Return: return 1 if c is lower ,else return zero
- *
+ *@c: value in ascii table
  */
 int _islower(int c)
 {
 
-	if (c <= 97 || c >=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
