3def37908b1c7ed4bbcd32838ad3993e5f64877b
TEjosee <tonyetim2.5@gmail.com>
2023-07-19 17:00:27 +0100

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_islower - checking Ascii value for c if lower or up
+ *
+ *Return: return 1 if c is lower ,else return zero
+ *
+ */
+int _islower(int c)
+{
+
+	if (c <= 97 || c >=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+
+}
