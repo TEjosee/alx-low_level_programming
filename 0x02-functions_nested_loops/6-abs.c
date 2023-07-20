c762ffbc33c908b69acdefddfcfb78f18bb3bb6b
TEjosee <tonyetim2.5@gmail.com>
2023-07-20 02:52:06 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - describing the function int and use to print
+ *absolute integer
+ *
+ *Return: return i if i is greater than or equal to z
+ *ero else return i*-1
+ *
+ * @i: variable used in the function
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	
+		return (i * -1);	
+}
