d915d6ad597ae24bb20646af23606ae4f4ea36ec
TEjosee <tonyetim2.5@gmail.com>
2023-07-20 10:58:53 +0100

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,16 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d = 0;
+	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
+				for (d = 0; d <= 9; d++)
+					{
 				if (a >= 2 && b >= 4)
 					break;
 				_putchar(a + 48);
@@ -26,4 +29,6 @@ void jack_bauer(void)
 			}
 		}
 	}
+	}
+	
 }
