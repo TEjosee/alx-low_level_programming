7b52a16071feebed65321d499b30972f86f1682d
TEjosee <tonyetim2.5@gmail.com>
2023-07-24 16:37:07 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *reset_to_98 - changing zhe value of n using pointer
+ *Return: Always zero for success
+ *
+ *@n: The name of the pointer used
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
