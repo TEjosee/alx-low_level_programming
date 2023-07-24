370acd9982fe00ec0b748426fe34dfcafa1fa50a
TEjosee <tonyetim2.5@gmail.com>
2023-07-24 03:42:29 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - the function for checking upper case letter
+ *@c: the varialbe being used in the parameter
+ *
+ *Return: return 1 if c is uppercase else return 0
+ */
+int _isupper(int c)
+{
+	if (c == 43)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
